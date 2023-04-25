e59abf315c56035d8369eb4d1b7799a19d72038f
Tu Dinh <dinhngoc.tu@irit.fr>
2023-04-25 19:03:12 +0200

    viewer: fix loading in previewer tab
    we do this by forcing emission of the `webviewerloaded` event at
    the current document

--- viewer/viewer.js
+++ viewer/viewer.js
@@ -13875,12 +13875,7 @@ function webViewerLoad() {
   event.initCustomEvent("webviewerloaded", true, true, {
     source: window
   });
-  try {
-    parent.document.dispatchEvent(event);
-  } catch (ex) {
-    // console.error(`webviewerloaded: ${ex}`);
-    document.dispatchEvent(event);
-  }
+  document.dispatchEvent(event);
   _app.PDFViewerApplication.run(config);
 }
 document.blockUnblockOnload?.(true);
